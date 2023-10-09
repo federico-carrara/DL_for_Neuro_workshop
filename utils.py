6f43287aadd1f2e0b0cd778db87d383fcff0f0b8
Federico Carrara <fcarrara@d.ethz.ch@bs-dw139.ethz.ch>
2023-10-09 23:44:15 +0200

    Fixed bugs

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import string
 from torch.utils.data import Dataset, DataLoader
 from scipy.io import loadmat
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 #--------------------------------------------------------------------------
 def load_eeg_data_file(
@@ -21,25 +21,25 @@ def load_eeg_data_file(
 #--------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------
-def windowize_data(
+def windowize_signal(
     data: List[torch.tensor],
     length: int,
     overlap: int
-) -> np.ndarray: 
+) -> Tuple[List[torch.tensor], List[int]]: 
 
     assert overlap < length, f"Window overlapping {overlap} cannot be greater than the window length {length}."
 
     windowized_data = []
+    num_windows = []
     for signal in data:
         ptr = 0
         while ptr + length < signal.shape[1]:
             windowized_data.append(signal[:, ptr:(ptr + length)])
             ptr += length - overlap
+        num_windows.append(len(windowized_data))
 
-    return windowized_data
+    return windowized_data, num_windows
 #--------------------------------------------------------------------------
-
-
 
 #--------------------------------------------------------------------------
 class TrainDataset(Dataset):
@@ -54,22 +54,22 @@ class TrainDataset(Dataset):
         # Notice: N_subjects = 15, N_recordings=45 (N_subjects*3)
         self.N_subjects = 15
         self.N_recordings = 45
+        self.N_channels = 62
         self.win_length = win_length
         self.win_overlap = win_overlap
 
-        # Load data store them in a torch tensor
+        # Load data and labels, store them in torch tensors
         eeg_files = [fname for fname in os.listdir(path_to_data_dir) if fname[0] in string.digits]
+        labels_dict = loadmat(path_to_labels)
+        labels = labels_dict["label"].squeeze(0)
         data = []
         for eeg_file in eeg_files:
             curr_data = load_eeg_data_file(os.path.join(path_to_data_dir, eeg_file))
-            curr_windowized_data = windowize_data(curr_data, self.win_length, self.win_overlap)
-            data.append(curr_windowized_data)
+            curr_windowized_data, num_windows = windowize_signal(curr_data, self.win_length, self.win_overlap)
+            curr_labels = [[labels[i]] * num_windows[i] for i in range(len(curr_data))]
+            data.extend(curr_windowized_data)
         self.data = torch.tensor(data)
-
-        # Load labels
-        labels_dict = loadmat(path_to_labels)
-        labels = torch.tensor(labels_dict["label"])
-        self.labels = labels.repeat(self.N_recordings, 1)
+        self.labels = curr_labels.repeat(self.N_recordings, 1, 1)
         
         # Boolean: if true apply transformation for data augmentation
         if do_augmentation:
@@ -89,8 +89,9 @@ class TrainDataset(Dataset):
 
         return sample, label
 
-    def add_gaussian_noise(self):
-        return
+    def add_gaussian_noise(self, data: torch.tensor):
+        noise = torch.normal(mean=0.0, std_dev=0.2, size=(data.shape[0], data.shape[1]))
+        return data + noise
 
 #--------------------------------------------------------------------------
 
