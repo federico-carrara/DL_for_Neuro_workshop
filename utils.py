5a5c11b5b840f47eeb425a85b6a70b3300ebadef
Federico Carrara <fcarrara@d.ethz.ch@bs-dw139.ethz.ch>
2023-10-10 10:40:55 +0200

    Debugged dataset

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 import matplotlib.pyplot as plt
 import string
+from tqdm import tqdm
 from torch.utils.data import Dataset, DataLoader
 from scipy.io import loadmat
 from typing import List, Optional, Tuple
@@ -33,12 +34,20 @@ def windowize_signal(
     num_windows = []
     for signal in data:
         ptr = 0
+        curr_windowized_data = []
         while ptr + length < signal.shape[1]:
-            windowized_data.append(signal[:, ptr:(ptr + length)])
+            curr_windowized_data.append(signal[:, ptr:(ptr + length)])
             ptr += length - overlap
-        num_windows.append(len(windowized_data))
+        num_windows.append(len(curr_windowized_data))
+        windowized_data.extend(curr_windowized_data)
 
     return windowized_data, num_windows
+#--------------------------------------------------------------------------
+
+#--------------------------------------------------------------------------
+def add_gaussian_noise(data: torch.tensor):
+    noise = torch.normal(0, 0.2, size=(data.shape[0], data.shape[1]))
+    return data + noise
 #--------------------------------------------------------------------------
 
 #--------------------------------------------------------------------------
@@ -63,13 +72,15 @@ class TrainDataset(Dataset):
         labels_dict = loadmat(path_to_labels)
         labels = labels_dict["label"].squeeze(0)
         data = []
-        for eeg_file in eeg_files:
+        for eeg_file in tqdm(eeg_files, desc="Loading data files"):
             curr_data = load_eeg_data_file(os.path.join(path_to_data_dir, eeg_file))
             curr_windowized_data, num_windows = windowize_signal(curr_data, self.win_length, self.win_overlap)
-            curr_labels = [[labels[i]] * num_windows[i] for i in range(len(curr_data))]
             data.extend(curr_windowized_data)
-        self.data = torch.tensor(data)
-        self.labels = curr_labels.repeat(self.N_recordings, 1, 1)
+            curr_labels = [torch.tensor([labels[i]] * num_windows[i]) for i in range(len(curr_data))]
+            curr_labels = torch.concat(curr_labels, axis=0)
+        data = [tens.unsqueeze(0) for tens in data]
+        self.data = torch.concat(data, axis=0)
+        self.labels = curr_labels.repeat(self.N_recordings)
         
         # Boolean: if true apply transformation for data augmentation
         if do_augmentation:
@@ -89,19 +100,9 @@ class TrainDataset(Dataset):
 
         return sample, label
 
-    def add_gaussian_noise(self, data: torch.tensor):
-        noise = torch.normal(mean=0.0, std_dev=0.2, size=(data.shape[0], data.shape[1]))
-        return data + noise
-
 #--------------------------------------------------------------------------
 
 def plot_eeg_data(
     data: np.ndarray
 ) -> None:
     return 
-
-
-
-def find_bad_channels(
-    data: np.ndarray
-) -> List[int]:
