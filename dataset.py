68cb00c6c59597ce62b19ef3bf8983884565853a
federico-carrara <fcarrara@d.ethz.ch@bs-dw139.ethz.ch>
2023-10-21 15:48:16 +0200

    Extended fixes to all the datasets

--- dataset.py
+++ dataset.py
@@ -22,17 +22,17 @@ class TrainDataset(Dataset):
         path_to_labels: str,
         win_length: int,
         win_overlap: int, 
-        num_subjects: Optional[int] = 15,
-        num_trials: Optional[int] = 3,
+        num_recordings: int,
+        num_movies: Optional[int] = 15,
         num_channels: Optional[int] = 62,
         sampling_frequency: Optional[int] = 200,
         data_augmentation: Optional[bool] = True,
         preprocess_de: Optional[bool] = True,
         band_frequencies: Optional[List[Tuple[int, int]]] = [(4, 7), (8, 13), (14, 30), (31, 50)] 
     ):
-        # Notice: N_subjects = 15, N_recordings=45 (N_subjects*3)
-        self.N_subjects = num_subjects
-        self.N_recordings = num_subjects * num_trials
+    
+        self.N_recordings = num_recordings
+        self.N_movies = num_movies
         self.N_channels = num_channels
         self.sampl_freq = sampling_frequency
         self.win_length = win_length
@@ -41,18 +41,25 @@ class TrainDataset(Dataset):
         # Load data and labels, store them in torch tensors
         eeg_files = [fname for fname in os.listdir(path_to_data_dir) if fname[0] in string.digits]
         labels_dict = loadmat(path_to_labels)
-        labels = labels_dict["label"].squeeze(0)
+        labels_unique = labels_dict["label"].squeeze(0)
         data = []
-        for eeg_file in tqdm(eeg_files, desc="Loading training data files"):
-            curr_data = load_eeg_data_file(os.path.join(path_to_data_dir, eeg_file))
+        labels = []
+        for eeg_file in tqdm(eeg_files, desc="Loading validation data files"):
+            curr_data = load_eeg_data_file(
+                path_to_file=os.path.join(path_to_data_dir, eeg_file),
+                num_keys=self.N_movies,
+            )
             curr_windowized_data, num_windows = windowize_signal(curr_data, self.win_length, self.win_overlap)
             data.extend(curr_windowized_data)
-            curr_labels = [torch.tensor([labels[i]] * num_windows[i]) for i in range(len(curr_data))]
-            curr_labels = torch.concat(curr_labels, axis=0)
+            curr_labels = [
+                torch.tensor([labels_unique[i]] * num_windows[i])
+                for i in range(len(curr_data))
+            ]
+            labels.append(torch.concat(curr_labels, axis=0))
         data = [tens.unsqueeze(0) for tens in data]
         self.data = torch.concat(data, axis=0).unsqueeze(1)
         self.data = self.data.type(torch.float32)
-        self.labels = curr_labels.repeat(self.N_recordings)
+        self.labels = torch.concat(labels)
         self.labels += 1
         self.labels = self.labels.type(torch.int64)
         
@@ -94,19 +101,17 @@ class ValidationDataset(Dataset):
         path_to_labels: str,
         win_length: int,
         win_overlap: int, 
-        num_subjects: Optional[int] = 15,
-        num_trials: Optional[int] = 3,
+        num_recordings: int,
         num_movies: Optional[int] = 15,
         num_channels: Optional[int] = 62,
         sampling_frequency: Optional[int] = 200,
         preprocess_de: Optional[bool] = True,
         band_frequencies: Optional[List[Tuple[int, int]]] = [(4, 7), (8, 13), (14, 30), (31, 50)] 
     ):
-        # Notice: N_subjects = 15, N_recordings=45 (N_subjects*3)
-        self.N_subjects = num_subjects
-        self.N_recordings = num_subjects * num_trials
-        self.N_channels = num_channels
+
+        self.N_recordings = num_recordings
         self.N_movies = num_movies
+        self.N_channels = num_channels
         self.sampl_freq = sampling_frequency
         self.win_length = win_length
         self.win_overlap = win_overlap
@@ -171,17 +176,16 @@ class TestDataset(Dataset):
         path_to_labels: str,
         win_length: int,
         win_overlap: int, 
-        num_subjects: Optional[int] = 15,
-        num_trials: Optional[int] = 3,
+        num_recordings: int,
+        num_movies: Optional[int] = 15,
         num_channels: Optional[int] = 62,
         sampling_frequency: Optional[int] = 200,
-        data_augmentation: Optional[bool] = True,
         preprocess_de: Optional[bool] = True,
         band_frequencies: Optional[List[Tuple[int, int]]] = [(4, 7), (8, 13), (14, 30), (31, 50)] 
     ):
-        # Notice: N_subjects = 15, N_recordings=45 (N_subjects*3)
-        self.N_subjects = num_subjects
-        self.N_recordings = num_subjects * num_trials
+
+        self.N_recordings = num_recordings
+        self.N_movies = num_movies
         self.N_channels = num_channels
         self.sampl_freq = sampling_frequency
         self.win_length = win_length
@@ -190,18 +194,25 @@ class TestDataset(Dataset):
         # Load data and labels, store them in torch tensors
         eeg_files = [fname for fname in os.listdir(path_to_data_dir) if fname[0] in string.digits]
         labels_dict = loadmat(path_to_labels)
-        labels = labels_dict["label"].squeeze(0)
+        labels_unique = labels_dict["label"].squeeze(0)
         data = []
-        for eeg_file in tqdm(eeg_files, desc="Loading test data files"):
-            curr_data = load_eeg_data_file(os.path.join(path_to_data_dir, eeg_file))
+        labels = []
+        for eeg_file in tqdm(eeg_files, desc="Loading validation data files"):
+            curr_data = load_eeg_data_file(
+                path_to_file=os.path.join(path_to_data_dir, eeg_file),
+                num_keys=self.N_movies,
+            )
             curr_windowized_data, num_windows = windowize_signal(curr_data, self.win_length, self.win_overlap)
             data.extend(curr_windowized_data)
-            curr_labels = [torch.tensor([labels[i]] * num_windows[i]) for i in range(len(curr_data))]
-            curr_labels = torch.concat(curr_labels, axis=0)
+            curr_labels = [
+                torch.tensor([labels_unique[i]] * num_windows[i])
+                for i in range(len(curr_data))
+            ]
+            labels.append(torch.concat(curr_labels, axis=0))
         data = [tens.unsqueeze(0) for tens in data]
         self.data = torch.concat(data, axis=0).unsqueeze(1)
         self.data = self.data.type(torch.float32)
-        self.labels = curr_labels.repeat(self.N_recordings)
+        self.labels = torch.concat(labels)
         self.labels += 1
         self.labels = self.labels.type(torch.int64)
 
