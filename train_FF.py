bf4a52407dacd24f1ce033b3f5bdf3d2b49cab5a
federico-carrara <fcarrara@d.ethz.ch@bs-dw139.ethz.ch>
2023-10-22 21:53:44 +0200

    Fixed bugs

--- train_FF.py
+++ train_FF.py
@@ -3,13 +3,13 @@ from dataset import TrainDataset, ValidationDataset, TestDataset
 from model import EEGFeedForwardNet
 import pytorch_lightning as pl
 
-window_length = 1000
-window_overlap = 100
-batch_sz = 16
+window_length = 2000
+window_overlap = 200
+batch_sz = 64
 model_params = {
     "input_size": 248,
     "num_classes": 3,
-    "dropout_prob": 0.3, 
+    "dropout_prob": 0.5, 
     "hidden_size": 64,
     "norm_method": "batch"
 }
@@ -17,19 +17,19 @@ training_params = {
     "lr": 5e-4, 
     "betas": [0.9, 0.99], 
     "weight_decay": 1e-6, 
-    "epochs": 100, 
+    "epochs": 20, 
+    "lr_patience": 3
 }
-
 
 train_dataset = TrainDataset(
     path_to_data_dir="../data/train/", 
     path_to_labels="../data/label.mat", 
     win_length=window_length, 
     win_overlap=window_overlap, 
-    data_augmentation=False,
     preprocess_de=True,
     normalization="trial",
-    map_to_grid=False
+    map_to_grid=False,
+    path_to_preprocessed=None
 )
 print("------------------------------------------------------------")
 val_dataset = ValidationDataset(
@@ -52,9 +52,9 @@ test_dataset = TestDataset(
 )
 print("------------------------------------------------------------")
 
-train_dataloader = DataLoader(train_dataset, batch_size=batch_sz, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=batch_sz, shuffle=False)
-test_dataloader = DataLoader(test_dataset, batch_size=batch_sz, shuffle=False)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_sz, shuffle=True, num_workers=8)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_sz, shuffle=False, num_workers=8)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_sz, shuffle=False, num_workers=8)
 
 eeg_net = EEGFeedForwardNet(
     model_parameters=model_params,
